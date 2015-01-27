a9de7d16f788b92aef0fe47674bf7c6222b7d6b1
Dominique Quatravaux <dominique@quatravaux.org>
2015-01-27 16:21:26 +0100

    Developer instructions

--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@
 
 # Usage:
 # wget -O - https://raw.githubusercontent.com/domq/epfl.openstack-sti.foreman/master/run.sh  | bash
+#
+# Please keep this script:
+#  * repeatable: it should be okay to run it twice
+#  * readable (with comments in english)
+#  * minimalistic: complicated things should be done with Puppet instead
 
 echo "Congratulations!!!!!!!!"
